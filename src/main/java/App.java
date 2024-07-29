8ad1a50085425883817d48da8b23674371427547
bulochkka <ram23zan@mail.ru>
2024-07-30 00:20:32 +0300

    Izmenil

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean2=(Cat) applicationContext.getBean("cat");
+        Cat bean3=(Cat) applicationContext.getBean("cat");
+        boolean f=bean.equals(bean1);
+        System.out.println(f);
+        boolean v=bean2.equals(bean3);
+        System.out.println(v);
     }
 }
